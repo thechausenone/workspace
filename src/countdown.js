584e28813a94d34f64d0315f6dc7ec24dc0a16c4
weihan <weihan@mail.uoguelph.ca>
2017-10-02 23:56:08 -0400

    base model

--- src/countdown.js
+++
@@ -1,9 +0,0 @@
-module.exports = function countdown(tick){
-    let count  = 3
-
-    let timer  = setInterval(() => {
-        tick(count--)
-        if (count === -1)
-            clearInterval(timer)
-    }, 1000)
-}
