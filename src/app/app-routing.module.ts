06dce6b3ed4fdc22330661700ca3abeff1540f84
thechausenone <thechausenone7@gmail.com>
2017-10-15 22:25:07 -0400

    Removed HomeComponent
    moved remaining functionality of HomeComponent to AppComponent (i.e., html/css)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,11 @@
-import { HomeComponent } from './components/home/home.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
     {
         path: '',
-        component: HomeComponent
+        component: AppComponent
     }
 ];
 
