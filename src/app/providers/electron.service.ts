9807bf10989813e2a073ffd37aad652316d029b3
David Chau <thechausenone7@gmail.com>
2017-12-31 21:19:22 -0500

    74 resize windows before opening (#100)
    * Minor changes to previous "open" functionality
    
    * Open, Resize, and Map exe files
    
    note: no support for .txt or regular image files yet (only windows as well)
    
    * Limited grid to 4x4
    
    Ensures that mapping is always consistent
    
    * Added support for other file types
    
    Credits to Jiwan

--- src/app/providers/electron.service.ts
+++ src/app/providers/electron.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@angular/core';
 // If you import a module but never use any of the imported values other than as TypeScript types,
 // the resulting javascript file will look as if you never imported the module at all.
-import { ipcRenderer } from 'electron';
+import { ipcRenderer, shell, screen } from 'electron';
 import * as childProcess from 'child_process';
 import { Board } from 'app/components/navbar/objects/board.object';
 import { Window } from 'app/components/grid/objects/window.object';
 
 @Injectable()
 export class ElectronService {
-
   ipcRenderer: typeof ipcRenderer;
   childProcess: typeof childProcess;
+  pathToBAT: string;
+  pathToNircmd: string;
+  screenWidth: number;
+  screenHeight: number;
+
+  //note: assume 4x4 grid
+  maxCols: number;
+  maxRows: number;
 
   constructor() {
     // Conditional imports
@@ -18,19 +25,71 @@ export class ElectronService {
       this.ipcRenderer = window.require('electron').ipcRenderer;
       this.childProcess = window.require('child_process');
     }
+
+    this.pathToBAT = __dirname + "\\assets\\scripts\\open-windows.bat";
+    this.pathToNircmd = __dirname + "\\assets\\scripts\\nircmd.exe";
+    this.screenHeight = screen.getPrimaryDisplay().size.height;
+    this.screenWidth = screen.getPrimaryDisplay().size.width;
+    this.maxCols = 4;
+    this.maxRows = 4;
   }
 
   isElectron = () => {
     return window && window.process && window.process.type;
   }
 
-  public openBoard(board:Board){
-      const {shell} = require('electron');
+  public activateBoard(board:Board): void{
       var windows = board.windows;
-      for(var i = 0; i < windows.length; i++){
-          var boardWindow:Window = windows[i];
-          shell.openItem(boardWindow.windowFilePath);
+      var scriptCommand = "@ECHO OFF";
+
+      for (let window of windows){
+        scriptCommand += this.createCommand(window);
       }
+      
+      this.writeToScript(scriptCommand);
+      this.executeScript();
+  }
+
+  private createCommand(window: Window): string{
+    if (window.windowFilePath === "" || window.windowFilePath === null){
+      return "";
+    }
+    
+    var path = window.windowFilePath;
+    var x = window.x * (this.screenWidth/this.maxRows);
+    var y = window.y * (this.screenHeight/this.maxCols);
+    var width = (window.cols/this.maxCols) * this.screenWidth;
+    var height = (window.rows/this.maxRows) * this.screenHeight;
+
+    var cmd = `
+              start "" "${path}" /n
+              ${this.pathToNircmd} wait 1000
+              ${this.pathToNircmd} win setsize foreground  ${x} ${y} ${width} ${height}
+              `;
+
+    return cmd;
+  }
+
+  private writeToScript(scriptContent: string): void{
+    var fs = require('file-system');
+    fs.writeFile(this.pathToBAT, scriptContent, (err) => {
+      if (err) {
+          console.log("An error ocurred when writing to the file" + err.message);
+          console.log(err);
+          return;
+      }
+  });
+  }
+
+  private executeScript(): void{
+    const { exec } = require('child_process');
+    exec(this.pathToBAT, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log(stdout);
+    });
   }
 
 }
