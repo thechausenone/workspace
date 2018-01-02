edf3507b20ab10c439b502772d8f36bbee0cb992
weihan <weihan@mail.uoguelph.ca>
2018-01-02 18:12:11 -0500

    added exit dialog

---
+++ src/app/modules/dialog/components/exit-dialog/exit-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { BrowserWindow } from 'electron';
+@Component({
+  selector: 'app-exit-dialog',
+  templateUrl: './exit-dialog.component.html',
+  styleUrls: ['./exit-dialog.component.scss']
+})
+export class ExitDialogComponent {
+
+  constructor(public dialogRef: MatDialogRef<ExitDialogComponent>,  @Inject(MAT_DIALOG_DATA) public data: any) {
+  }
+
+  cancelDialog(): void {
+    this.dialogRef.close();
+  }
+
+  quitApp(): void{
+    this.data.close();
+  }
+
+}
--- src/app/modules/dialog/dialog.module.ts
+++ src/app/modules/dialog/dialog.module.ts
@@ -3,12 +3,14 @@ import { SharedModule } from '../shared/shared.module';
 import { BoardDialogComponent } from './components/board-dialog/board-dialog.component';
 import { WindowDialogComponent } from './components/window-dialog/window-dialog.component';
 import { BoardSettingsDialogComponent } from './components/board-settings-dialog/board-settings-dialog.component';
+import { ExitDialogComponent } from './components/exit-dialog/exit-dialog.component';
 
 @NgModule({
   declarations: [
     BoardDialogComponent,
     WindowDialogComponent,
-    BoardSettingsDialogComponent
+    BoardSettingsDialogComponent,
+    ExitDialogComponent
   ],
   imports: [
     SharedModule
@@ -16,12 +18,14 @@ import { BoardSettingsDialogComponent } from './components/board-settings-dialog
   exports: [
     BoardDialogComponent,
     WindowDialogComponent,
-    BoardSettingsDialogComponent
+    BoardSettingsDialogComponent,
+    ExitDialogComponent
   ],
   entryComponents: [
     BoardDialogComponent,
     WindowDialogComponent,
-    BoardSettingsDialogComponent
+    BoardSettingsDialogComponent,
+    ExitDialogComponent
   ],
 })
 export class DialogModule {}
